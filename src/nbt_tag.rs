f81a37281c3f41203d14a04eaf302f55c04fbd99
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-27 15:34:47 +0000

    Implement tag_type for NbtTag

--- src/nbt_tag.rs
+++ src/nbt_tag.rs
@@ -13,3 +13,23 @@ pub enum NbtTag {
     IntArray(Vec<i32>),
     LongArray(Vec<i64>),
 }
+
+impl NbtTag {
+    pub fn tag_type(&self) -> u8 {
+        match self {
+            NbtTag::End => 0x00,
+            NbtTag::Byte(_) => 0x01,
+            NbtTag::Short(_) => 0x02,
+            NbtTag::Int(_) => 0x03,
+            NbtTag::Long(_) => 0x04,
+            NbtTag::Float(_) => 0x05,
+            NbtTag::Double(_) => 0x06,
+            NbtTag::ByteArray(_) => 0x07,
+            NbtTag::String(_) => 0x08,
+            NbtTag::List(_) => 0x09,
+            NbtTag::Compound(_) => 0x0a,
+            NbtTag::IntArray(_) => 0x0b,
+            NbtTag::LongArray(_) => 0x0c,
+        }
+    }
+}
