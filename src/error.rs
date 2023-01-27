ed9cedffe1b9ec43f78d618db8fdafcf56724e2f
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-27 17:33:25 +0000

    Remove serde

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,8 @@
 use std::{io, fmt, error};
 
-use serde::{ser, de};
-
 #[derive(Debug)]
 pub enum Error {
     Io(io::Error),
-    Serde
 }
 
 impl From<io::Error> for Error {
@@ -18,24 +15,11 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Error::Io(error) => write!(f, "IO error: {}", error),
-            Error::Serde => write!(f, "Serde error"),
         }
     }
 }
 
 impl error::Error for Error {}
 
-
-impl de::Error for Error {
-    fn custom<T: fmt::Display>(msg: T) -> Error {
-        Error::Serde
-    }
-}
-
-impl ser::Error for Error {
-    fn custom<T: fmt::Display>(msg: T) -> Error {
-        Error::Serde
-    }
-}
 
 pub type Result<T> = std::result::Result<T, Error>;
