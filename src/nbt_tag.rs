98b46c0e572d13d04990389cfd781620f61dcf2a
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-27 15:34:10 +0000

    Add missing tags

--- src/nbt_tag.rs
+++ src/nbt_tag.rs
@@ -1,15 +1,15 @@
 pub enum NbtTag {
+    End,
     Byte(u8),
-    Boolean(bool),
     Short(i16),
     Int(i32),
     Long(i64),
     Float(f32),
     Double(f64),
+    ByteArray(Vec<u8>),
     String(String),
     List(Vec<NbtTag>),
     Compound(Vec<(String, NbtTag)>),
-    ByteArray(Vec<u8>),
     IntArray(Vec<i32>),
     LongArray(Vec<i64>),
 }
