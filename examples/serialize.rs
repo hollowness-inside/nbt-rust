8a598d071730a57ae12f9cb91e9654d239a0b29f
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-28 17:35:36 +0000

    Fix

--- examples/serialize.rs
+++ examples/serialize.rs
@@ -1,16 +1,9 @@
 use std::io;
 
 use rnbt::Serializer;
-use serde::ser::SerializeSeq;
-use serde::Serializer as _;
 
 fn main() {
     let mut writer = io::stdout();
     let mut ser = Serializer::new(&mut writer);
-
-    let seq = ser.serialize_seq(Some(3)).unwrap();
-    let seq = seq.as_long_array();
-    seq.serialize_element(&123);
-    
-    seq.end().unwrap();
+    ser.serialize_f32(23.3);
 }
