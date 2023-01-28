c46c4da35dacecae80b1093b68c3f1267cff08d1
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-28 18:43:43 +0000

    Adapt the example

--- examples/serialize.rs
+++ examples/serialize.rs
@@ -1,9 +1,6 @@
-use std::io;
-
 use rnbt::Serializer;
 
 fn main() {
-    let mut writer = io::stdout();
-    let mut ser = Serializer::new(&mut writer);
-    ser.serialize_f32(23.3).unwrap();
+    let a = Serializer::serialize_bool(false).unwrap();
+    println!("{:?}", a);
 }
