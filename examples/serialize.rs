ad1fbd0864129ac0d0a605e1918f9a6c9194b139
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-28 15:14:35 +0000

    Create a working example

--- examples/serialize.rs
+++ examples/serialize.rs
@@ -1,12 +1,15 @@
 use std::io;
 
 use rnbt::Serializer;
-
+use serde::Serializer as _;
+use serde::ser::SerializeSeq;
 
 fn main() {
     let mut writer = io::stdout();
     let mut ser = Serializer::new(&mut writer);
-    // ser.serialize_string(" World").unwrap();
-    // ser.serialize_float(5.23).unwrap();
-    ser.serialize("Hello World").unwrap();
+
+    let seq = ser.serialize_seq(Some(3)).unwrap();
+    let seq = seq.set_type(ByteArray);
+    seq.serialize_element(123);
+    seq.end().unwrap();
 }
