6082aa41864d2bab3d421cfbae3383719ef58159
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-28 18:50:20 +0000

    Fix

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -1,16 +1,7 @@
-use std::io::Cursor;
-
 use rnbt::Serializer;
 
 fn main() {
-    let v = vec![0;128];
-    let mut c = Cursor::new(v);
-
-    let mut ser = Serializer::new(&mut c);
-    ser.serialize_int_array(&vec![1,2,3,4,5]).unwrap();
-
-    c.set_position(0);
-
-    let de = rnbt::de::from_reader(&mut c).unwrap();
+    let c = Serializer::serialize_int_array(&vec![1,2,3,4,5]).unwrap();
+    let de = rnbt::de::from_bytes(&c).unwrap();
     println!("{de:?}")
 }
