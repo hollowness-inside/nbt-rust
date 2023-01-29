e6ba881e96ef2681619562dc2ba6b9be73306b4c
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-29 09:55:27 +0000

    Stylize

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -3,20 +3,14 @@ use rnbt::NbtTag;
 use rnbt::de;
 
 fn main() {
-    let c = Serializer::serialize_int_array(&vec![1,2,3,4,5]).unwrap();
-    let c = Serializer::serialize_compound(
+    let bytes = Serializer::serialize_compound(
         &vec![
-            ("a".to_string(), NbtTag::Int(1)),
-            ("b".to_string(), NbtTag::Long(1)),
-            ("c".to_string(), NbtTag::String("Hello World".to_string())),
+            ("item_1".to_string(), NbtTag::Int(1)),
+            ("item_2".to_string(), NbtTag::Long(1)),
+            ("item_3".to_string(), NbtTag::String("Hello World".to_string())),
             ]
         ).unwrap();
-    let de = de::from_bytes(&c).unwrap();
-    println!("{de:?}");
 
-    let mut c = Serializer::start_compound();
-    c.write_field("cat", &123).unwrap();
-    c.write_field("trophy", &"strong".to_string()).unwrap();
-    let c = c.end();
-    println!("{:?}", c);
+    let compound = de::from_bytes(&bytes).unwrap();
+    println!("{compound:?}");
 }
