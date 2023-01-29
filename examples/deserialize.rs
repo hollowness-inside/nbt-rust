873ffa7b2f8bcef8618fe770915a20bcf7c061ec
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-29 09:44:54 +0000

    Extend the example

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -1,7 +1,22 @@
 use rnbt::Serializer;
+use rnbt::NbtTag;
+use rnbt::de;
 
 fn main() {
     let c = Serializer::serialize_int_array(&vec![1,2,3,4,5]).unwrap();
-    let de = rnbt::de::from_bytes(&c).unwrap();
-    println!("{de:?}")
+    let c = Serializer::serialize_compound(
+        &vec![
+            ("a".to_string(), NbtTag::Int(1)),
+            ("b".to_string(), NbtTag::Long(1)),
+            ("c".to_string(), NbtTag::String("Hello World".to_string())),
+            ]
+        ).unwrap();
+    let de = de::from_bytes(&c).unwrap();
+    println!("{de:?}");
+
+    let mut c = Serializer::start_compound();
+    c.write_field("cat", &123).unwrap();
+    c.write_field("trophy", &"strong".to_string()).unwrap();
+    let c = c.end();
+    println!("{:?}", c);
 }
