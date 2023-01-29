4f56670d199bb4a1ea6d440e089a6db548f81739
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-29 10:27:10 +0000

    Remove unnecessary type annotation

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -3,7 +3,7 @@ use rnbt::NbtTag;
 use rnbt::Serializer;
 
 fn main() {
-    let bytes = Serializer::serialize_compound(&vec![
+    let bytes = Serializer::serialize_compound(&[
         ("item_1", NbtTag::Int(1)),
         ("item_2", NbtTag::Long(1)),
         ("item_3", NbtTag::String("Hello World".to_string())),
