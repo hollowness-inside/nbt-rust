4d35b380d54f4fdad6ef2dc1e2185f1aae451865
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-29 10:25:50 +0000

    Cargo fmt

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -1,14 +1,14 @@
-use rnbt::Serializer;
-use rnbt::NbtTag;
 use rnbt::de;
+use rnbt::NbtTag;
+use rnbt::Serializer;
 
 fn main() {
-    let bytes = Serializer::serialize_compound(
-        &vec![
-            ("item_1", NbtTag::Int(1)),
-            ("item_2", NbtTag::Long(1)),
-            ("item_3", NbtTag::String("Hello World".to_string())),
-        ]).unwrap();
+    let bytes = Serializer::serialize_compound(&vec![
+        ("item_1", NbtTag::Int(1)),
+        ("item_2", NbtTag::Long(1)),
+        ("item_3", NbtTag::String("Hello World".to_string())),
+    ])
+    .unwrap();
 
     let compound = de::from_bytes(&bytes).unwrap();
     println!("{compound:?}");
