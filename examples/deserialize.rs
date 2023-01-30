905f961208da3d5ea70dc0b553e58d0444691087
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-30 13:57:36 +0000

    Use ? to handle Results

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -1,21 +1,23 @@
 use std::io::Cursor;
 
-use rnbt::{de::from_bytes, Serializer};
+use rnbt::{de::from_bytes, error::Result, Serializer};
 
-fn main() {
+fn main() -> Result<()> {
     // Some binary NBT data to deserialize
-    let data = data();
+    let data = data()?;
 
     // Deserializes the data
-    let deserialized = from_bytes(&data).unwrap();
+    let deserialized = from_bytes(&data)?;
 
     // Prints the parsed NBT data
     println!("{deserialized:#?}");
+
+    Ok(())
 }
 
 // Copied from examples/serialize.rs
 // Generates some test NBT data, NBT compound tag with 3 fields.
-fn data() -> Vec<u8> {
+fn data() -> Result<Vec<u8>> {
     let writer = Cursor::new(Vec::new());
 
     // Creates a new serializer with the given writer.
@@ -26,15 +28,14 @@ fn data() -> Vec<u8> {
     let mut comp = ser.start_compound();
 
     // Writes fields to the compound tag.
-    comp.write_field("item_1", 123u8).unwrap();
-    comp.write_field("item_2", "Hello World".to_string())
-        .unwrap();
-    comp.write_field("item_3", vec![1i32, 2, 3, 4, 5]).unwrap();
+    comp.write_field("item_1", 123u8)?;
+    comp.write_field("item_2", "Hello World".to_string())?;
+    comp.write_field("item_3", vec![1i32, 2, 3, 4, 5])?;
 
     // Ends the compound tag and returns the serializer.
-    let ser = comp.end().unwrap();
+    let ser = comp.end()?;
 
     // Prints the value of the owned writer.
     // into_inner is called twice because the Vec is wrapped in a Cursor.
-    ser.into_inner().into_inner()
+    Ok(ser.into_inner().into_inner())
 }
--- examples/serialize.rs
+++ examples/serialize.rs
@@ -1,8 +1,8 @@
 use std::io::Cursor;
 
-use rnbt::Serializer;
+use rnbt::{error::Result, Serializer};
 
-fn main() {
+fn main() -> Result<()> {
     let writer = Cursor::new(Vec::new());
 
     // Creates a new serializer with the given writer.
@@ -13,14 +13,15 @@ fn main() {
     let mut comp = ser.start_compound();
 
     // Writes fields to the compound tag.
-    comp.write_field("item_1", 123u8).unwrap();
-    comp.write_field("item_2", "Hello World".to_string()).unwrap();
-    comp.write_field("item_3", vec![1i32, 2, 3, 4, 5]).unwrap();
+    comp.write_field("item_1", 123u8)?;
+    comp.write_field("item_2", "Hello World".to_string())?;
+    comp.write_field("item_3", vec![1i32, 2, 3, 4, 5])?;
 
     // Ends the compound tag and returns the serializer.
-    let ser = comp.end().unwrap();
+    let ser = comp.end()?;
 
     // Prints the value of the owned writer.
     // into_inner is called twice because the Vec is wrapped in a Cursor.
     println!("{:?}", ser.into_inner().into_inner());
+    Ok(())
 }
