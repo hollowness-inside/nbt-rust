7d09005bbd46f2866240ffd49221b6b49866946c
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-30 13:39:43 +0000

    Adapt the example to the new approach

--- examples/serialize.rs
+++ examples/serialize.rs
@@ -1,6 +1,9 @@
+use std::io::Cursor;
+
 use rnbt::Serializer;
 
 fn main() {
-    let a = Serializer::serialize_bool(false).unwrap();
-    println!("{a:?}");
+    let mut ser = Serializer::new(Cursor::new(Vec::new()));
+    ser.serialize_byte_array(&[1,2,3,4,5]).unwrap();
+    println!("{:?}", ser.into_inner().into_inner());
 }
