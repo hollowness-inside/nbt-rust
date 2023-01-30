4696b9447829befa2016297e7e2a33c993ad1f37
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-30 13:46:12 +0000

    Add comments

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -4,14 +4,23 @@ use rnbt::Serializer;
 
 fn main() {
     let writer = Cursor::new(Vec::new());
+
+    // Creates a new serializer with the given writer.
+    // Serializer is capable of writing NBT Tags to the writer.
     let ser = Serializer::new(writer);
 
+    // Starts a new compound tag.
     let mut comp = ser.start_compound();
+
+    // Writes fields to the compound tag.
     comp.write_field("item_1", 123u8).unwrap();
     comp.write_field("item_2", "Hello World".to_string()).unwrap();
     comp.write_field("item_3", vec![1i32, 2, 3, 4, 5]).unwrap();
+
+    // Ends the compound tag and returns the serializer.
     let ser = comp.end().unwrap();
 
-    let compound = ser.into_inner().into_inner();
-    println!("{compound:?}");
+    // Prints the value of the owned writer.
+    // into_inner is called twice because the Vec is wrapped in a Cursor.
+    println!("{:?}", ser.into_inner().into_inner());
 }
