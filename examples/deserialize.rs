bba8ea809f2c3b3244d28253362df1ff895633ef
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-30 13:36:41 +0000

    Adapt the example to the new approach

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -1,15 +1,17 @@
-use rnbt::de;
-use rnbt::NbtTag;
+use std::io::Cursor;
+
 use rnbt::Serializer;
 
 fn main() {
-    let bytes = Serializer::serialize_compound(&[
-        ("item_1", NbtTag::Int(1)),
-        ("item_2", NbtTag::Long(1)),
-        ("item_3", NbtTag::String("Hello World".to_string())),
-    ])
-    .unwrap();
+    let writer = Cursor::new(Vec::new());
+    let ser = Serializer::new(writer);
 
-    let compound = de::from_bytes(&bytes).unwrap();
+    let mut comp = ser.start_compound();
+    comp.write_field("item_1", 123u8).unwrap();
+    comp.write_field("item_2", "Hello World".to_string()).unwrap();
+    comp.write_field("item_3", vec![1i32, 2, 3, 4, 5]).unwrap();
+    let ser = comp.end().unwrap();
+
+    let compound = ser.into_inner().into_inner();
     println!("{compound:?}");
 }
