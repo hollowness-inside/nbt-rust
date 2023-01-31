227848f0c2c42e44cc812646c2cc29b745acc538
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-31 12:53:35 +0000

    Update

--- examples/from_file.rs
+++ examples/from_file.rs
@@ -1,7 +1,10 @@
 use nbt_rust::{de::from_bytes, error::Result};
 
 fn main() -> Result<()> {
+    // Well I'm a bit cheating here
+    // I ungzipped the file beforehand
     let file = include_bytes!("file.nbt");
+    
     let (name, value) = from_bytes(file)?;
     print!("Name: {name}");
     println!("Value: {value}");
