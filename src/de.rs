0b181b89fd7ef87ceb72c47a54e59416f66b3fe5
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-01-31 06:05:55 +0000

    Update Deserialization method

--- src/de.rs
+++ src/de.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{Cursor, Read};
 
 use crate::{
     error::{Error, Result},
@@ -7,138 +7,195 @@ use crate::{
 };
 
 /// Reads a single NBT tag from a reader
-pub fn from_reader<R: Read>(reader: &mut R) -> Result<NbtTag> {
-    let mut bytes = reader.bytes();
-    if let Some(Ok(byte)) = bytes.next() {
-        match byte {
-            prefixes::BYTE => {
-                let mut buf = [0; 1];
-                reader.read_exact(&mut buf)?;
-                Ok(NbtTag::Byte(buf[0]))
-            }
-            prefixes::SHORT => {
-                let mut buf = [0; 2];
-                reader.read_exact(&mut buf)?;
-                Ok(NbtTag::Short(i16::from_be_bytes(buf)))
-            }
-            prefixes::INT => {
-                let mut buf = [0; 4];
-                reader.read_exact(&mut buf)?;
-                Ok(NbtTag::Int(i32::from_be_bytes(buf)))
-            }
-            prefixes::LONG => {
-                let mut buf = [0; 8];
-                reader.read_exact(&mut buf)?;
-                Ok(NbtTag::Long(i64::from_be_bytes(buf)))
-            }
-            prefixes::FLOAT => {
-                let mut buf = [0; 4];
-                reader.read_exact(&mut buf)?;
-                Ok(NbtTag::Float(f32::from_be_bytes(buf)))
-            }
-            prefixes::DOUBLE => {
-                let mut buf = [0; 8];
-                reader.read_exact(&mut buf)?;
-                Ok(NbtTag::Double(f64::from_be_bytes(buf)))
-            }
-            prefixes::BYTE_ARRAY => {
-                let mut buf = [0; 4];
-                reader.read_exact(&mut buf)?;
-                let len = i32::from_be_bytes(buf) as usize;
-                let mut bytes = vec![0; len];
-                reader.read_exact(&mut bytes)?;
-                Ok(NbtTag::ByteArray(bytes))
-            }
-            prefixes::STRING => {
-                let mut buf = [0; 2];
-                reader.read_exact(&mut buf)?;
-                let len = i16::from_be_bytes(buf) as usize;
+pub fn from_reader<R: Read>(reader: &mut R) -> Result<(String, NbtTag)> {
+    let (prefix, name) = read_tag_header(reader)?;
+    let value = match prefix {
+        prefixes::BYTE => read_headless_byte(reader)?,
+        prefixes::SHORT => read_headless_short(reader)?,
+        prefixes::INT => read_headless_int(reader)?,
+        prefixes::LONG => read_headless_long(reader)?,
+        prefixes::FLOAT => read_headless_float(reader)?,
+        prefixes::DOUBLE => read_headless_double(reader)?,
+        prefixes::BYTE_ARRAY => read_headless_byte_array(reader)?,
+        prefixes::STRING => read_headless_string(reader)?,
+        prefixes::LIST => read_headless_list(reader)?,
+        prefixes::COMPOUND => read_headless_compound(reader)?,
+        prefixes::INT_ARRAY => read_headless_int_array(reader)?,
+        prefixes::LONG_ARRAY => read_headless_long_array(reader)?,
+        _ => return Err(Error::UnknownTagType(prefix)),
+    };
 
-                let mut bytes = vec![0; len];
-                reader.read_exact(&mut bytes)?;
-                Ok(NbtTag::String(String::from_utf8(bytes)?))
-            }
-            prefixes::LIST => {
-                let mut buf = [0; 1];
-                reader.read_exact(&mut buf)?;
-
-                let mut buf = [0; 4];
-                reader.read_exact(&mut buf)?;
-                let len = i32::from_be_bytes(buf) as usize;
-
-                let mut tags = Vec::with_capacity(len);
-                for _ in 0..len {
-                    tags.push(from_reader(reader)?);
-                }
-                Ok(NbtTag::List(tags))
-            }
-            prefixes::COMPOUND => {
-                let mut tags = Vec::new();
-
-                loop {
-                    let key = match from_reader(reader)? {
-                        NbtTag::String(name) => name,
-                        NbtTag::End => break,
-                        _ => return Err(Error::Generic("Expected string".to_string())),
-                    };
-
-                    let value = from_reader(reader)?;
-                    tags.push((key, value));
-                }
-                Ok(NbtTag::Compound(tags))
-            }
-            prefixes::INT_ARRAY => {
-                let mut buf = [0; 4];
-                reader.read_exact(&mut buf)?;
-                let len = i32::from_be_bytes(buf) as usize;
-
-                let mut bytes = vec![0; len * 4];
-                reader.read_exact(&mut bytes)?;
-                let mut ints = Vec::with_capacity(len);
-
-                for i in 0..len {
-                    ints.push(i32::from_be_bytes([
-                        bytes[i * 4],
-                        bytes[i * 4 + 1],
-                        bytes[i * 4 + 2],
-                        bytes[i * 4 + 3],
-                    ]));
-                }
-                Ok(NbtTag::IntArray(ints))
-            }
-            prefixes::LONG_ARRAY => {
-                let mut buf = [0; 4];
-                reader.read_exact(&mut buf)?;
-                let len = i32::from_be_bytes(buf) as usize;
-
-                let mut bytes = vec![0; len * 8];
-                reader.read_exact(&mut bytes)?;
-                let mut longs = Vec::with_capacity(len);
-
-                for i in 0..len {
-                    longs.push(i64::from_be_bytes([
-                        bytes[i * 8],
-                        bytes[i * 8 + 1],
-                        bytes[i * 8 + 2],
-                        bytes[i * 8 + 3],
-                        bytes[i * 8 + 4],
-                        bytes[i * 8 + 5],
-                        bytes[i * 8 + 6],
-                        bytes[i * 8 + 7],
-                    ]));
-                }
-                Ok(NbtTag::LongArray(longs))
-            }
-            prefixes::END => Ok(NbtTag::End),
-            _ => Err(Error::UnknownTagType(byte)),
-        }
-    } else {
-        Err(Error::Generic("Empty stream".to_string()))
-    }
+    return Ok((name, value));
 }
 
 /// Reads a single NBT tag from a byte slice
-pub fn from_bytes(bytes: &[u8]) -> Result<NbtTag> {
-    let mut reader = std::io::Cursor::new(bytes);
+pub fn from_bytes(bytes: &[u8]) -> Result<(String, NbtTag)> {
+    let mut reader = Cursor::new(bytes);
     from_reader(&mut reader)
+}
+
+fn read_tag_header<R: Read>(reader: &mut R) -> Result<(u8, String)> {
+    let mut prefix = [0; 1];
+    reader.read_exact(&mut prefix)?;
+    let prefix = prefix[0];
+
+    if prefix == prefixes::END {
+        return Ok((prefix, String::new()));
+    }
+
+    let mut name_len = [0; 2];
+    reader.read_exact(&mut name_len)?;
+    let name_len = u16::from_be_bytes(name_len);
+
+    let mut name = vec![0; name_len as usize];
+    reader.read_exact(&mut name)?;
+    let name = String::from_utf8(name)?;
+
+    Ok((prefix, name))
+}
+
+fn read_headless_byte<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 1];
+    reader.read_exact(&mut buf)?;
+
+    Ok(NbtTag::Byte(buf[0]))
+}
+
+fn read_headless_short<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 2];
+    reader.read_exact(&mut buf)?;
+
+    Ok(NbtTag::Short(i16::from_be_bytes(buf)))
+}
+
+fn read_headless_int<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 4];
+    reader.read_exact(&mut buf)?;
+
+    Ok(NbtTag::Int(i32::from_be_bytes(buf)))
+}
+
+fn read_headless_long<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 8];
+    reader.read_exact(&mut buf)?;
+
+    Ok(NbtTag::Long(i64::from_be_bytes(buf)))
+}
+
+fn read_headless_float<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 4];
+    reader.read_exact(&mut buf)?;
+
+    Ok(NbtTag::Float(f32::from_be_bytes(buf)))
+}
+
+fn read_headless_double<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 8];
+    reader.read_exact(&mut buf)?;
+
+    Ok(NbtTag::Double(f64::from_be_bytes(buf)))
+}
+
+fn read_headless_byte_array<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 4];
+    reader.read_exact(&mut buf)?;
+    let len = i32::from_be_bytes(buf);
+
+    let mut bytes = vec![0; len as usize];
+    reader.read_exact(&mut bytes)?;
+
+    Ok(NbtTag::ByteArray(bytes))
+}
+
+fn read_headless_string<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 2];
+    reader.read_exact(&mut buf)?;
+    let len = u16::from_be_bytes(buf);
+
+    let mut bytes = vec![0; len as usize];
+    reader.read_exact(&mut bytes)?;
+    let string = String::from_utf8(bytes)?;
+
+    Ok(NbtTag::String(string))
+}
+
+fn read_headless_list<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 1];
+    reader.read_exact(&mut buf)?;
+    let prefix = buf[0];
+
+    let mut buf = [0; 4];
+    reader.read_exact(&mut buf)?;
+    let len = i32::from_be_bytes(buf);
+
+    let mut tags = Vec::new();
+    for _ in 0..len {
+        tags.push(read_headless_tag(reader, prefix)?);
+    }
+
+    Ok(NbtTag::List(tags))
+}
+
+fn read_headless_compound<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut tags: Vec<(String, NbtTag)> = Vec::new();
+
+    loop {
+        let (prefix, name) = read_tag_header(reader)?;
+        if prefix == prefixes::END {
+            break;
+        }
+
+        let value = read_headless_tag(reader, prefix)?;
+
+        tags.push((name, value));
+    }
+    Ok(NbtTag::Compound(tags))
+}
+
+fn read_headless_int_array<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 4];
+    reader.read_exact(&mut buf)?;
+    let len = i32::from_be_bytes(buf);
+
+    let mut ints = Vec::new();
+    for _ in 0..len {
+        let mut buf = [0; 4];
+        reader.read_exact(&mut buf)?;
+        ints.push(i32::from_be_bytes(buf));
+    }
+
+    Ok(NbtTag::IntArray(ints))
+}
+
+fn read_headless_long_array<R: Read>(reader: &mut R) -> Result<NbtTag> {
+    let mut buf = [0; 4];
+    reader.read_exact(&mut buf)?;
+    let len = i32::from_be_bytes(buf);
+
+    let mut longs = Vec::new();
+    for _ in 0..len {
+        let mut buf = [0; 8];
+        reader.read_exact(&mut buf)?;
+        longs.push(i64::from_be_bytes(buf));
+    }
+
+    Ok(NbtTag::LongArray(longs))
+}
+
+fn read_headless_tag<R: Read>(reader: &mut R, prefix: u8) -> Result<NbtTag> {
+    match prefix {
+        prefixes::BYTE => read_headless_byte(reader),
+        prefixes::SHORT => read_headless_short(reader),
+        prefixes::INT => read_headless_int(reader),
+        prefixes::LONG => read_headless_long(reader),
+        prefixes::FLOAT => read_headless_float(reader),
+        prefixes::DOUBLE => read_headless_double(reader),
+        prefixes::BYTE_ARRAY => read_headless_byte_array(reader),
+        prefixes::STRING => read_headless_string(reader),
+        prefixes::LIST => read_headless_list(reader),
+        prefixes::COMPOUND => read_headless_compound(reader),
+        prefixes::INT_ARRAY => read_headless_int_array(reader),
+        prefixes::LONG_ARRAY => read_headless_long_array(reader),
+        _ => Err(Error::UnknownTagType(prefix)),
+    }
 }
