cdf990fbd61ee77e8e4336817a3e13b489f46de2
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-02-04 18:16:33 +0000

    impl ser::SerializeStruct

--- src/ser/map_serializer.rs
+++ src/ser/map_serializer.rs
@@ -52,15 +52,17 @@ impl<'a, W: io::Write> ser::SerializeStruct for MapSerializer<'a, W> {
     type Ok = ();
     type Error = Error;
 
-    fn serialize_field<T: ?Sized>(&mut self, _key: &'static str, _value: &T) -> Result<()>
+    fn serialize_field<T: ?Sized>(&mut self, key: &'static str, value: &T) -> Result<()>
     where
         T: serde::Serialize,
     {
-        todo!()
+        <Self as ser::SerializeMap>::serialize_key(self, key)?;
+        <Self as ser::SerializeMap>::serialize_value(self, value)?;
+        Ok(())
     }
 
     fn end(self) -> Result<()> {
-        todo!()
+        <Self as ser::SerializeMap>::end(self)
     }
 }
 
