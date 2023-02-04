84f178a242d38d04a0654f1e1f31818d17dbe6f4
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-02-04 18:16:43 +0000

    Update example

--- examples/serialize.rs
+++ examples/serialize.rs
@@ -1,32 +1,35 @@
 use std::io::Cursor;
+use nbt_rust::to_writer;
 
-use nbt_rust::{error::Result, ser::Serializer};
+#[derive(serde::Serialize)]
+struct GameData {
+    name: String,
+    version: u32,
+    player: Player
+}
 
-fn main() -> Result<()> {
-    let writer = Cursor::new(Vec::new());
+#[derive(serde::Serialize)]
+struct Player {
+    name: String,
+    health: u32,
+    // position: (f32, f32, f32)
+}
 
-    // Creates a new serializer with the given writer.
-    // Serializer is capable of writing NBT Tags to the writer.
-    let ser = Serializer::new(writer);
+fn main() {
+    let buffer: Vec<u8> = Vec::new();
+    let mut writer = Cursor::new(buffer);
 
-    // Starts a new compound tag.
-    let mut comp = ser.start_compound("my_compound")?;
+    let game_data = GameData {
+        name: "My Game".to_string(),
+        version: 1,
+        player: Player {
+            name: "Player 1".to_string(),
+            health: 100,
+            // position: (0.0, 0.0, 0.0)
+        }
+    };
 
-    // Writes fields to the compound tag.
-    comp.write_field("item_1", 123u8)?;
-    comp.write_field("item_2", "Hello World".to_string())?;
-    comp.write_field("item_3", vec![1i32, 2, 3, 4, 5])?;
+    to_writer(&mut writer, &game_data).unwrap();
 
-    // Ends the compound tag and returns the serializer.
-    let mut ser = comp.end()?;
-
-    // Usually there's no need to write anything else after a compound tag.
-    // However, if you want to write more tags, you can do so
-    ser.serialize_byte("my_byte", 82)?;
-    ser.serialize_short("your_short", 28)?;
-
-    // Prints the value of the owned writer.
-    // into_inner is called twice because the Vec is wrapped in a Cursor.
-    println!("{:?}", ser.into_inner().into_inner());
-    Ok(())
+    println!("{:?}", String::from_utf8(writer.into_inner()).unwrap());
 }
