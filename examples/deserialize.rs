004574a09ee160a138caf4c0e2e64684429e37d1
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-02-08 04:08:32 +0000

    Create from_bytes function

--- examples/deserialize.rs
+++ examples/deserialize.rs
@@ -1,8 +1,7 @@
-use nbt_rust::{de::from_reader, NbtTag};
+use nbt_rust::{de::from_bytes, NbtTag};
 
 fn main() {
     let j = b"\x03\x00\x02hi\x00\x00\x00\x12";
-    let reader = std::io::Cursor::new(j);
-    let d: (String, NbtTag) = from_reader(reader).unwrap();
+    let d: (String, NbtTag) = from_bytes(j).unwrap();
     println!("{:} {:}", d.0, d.1);
 }
