58bb3e0021ee45a4b12e5df8df0bff7f3326545f
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-02-08 03:01:12 +0000

    Create from_reader for Deserializer

--- src/de.rs
+++ src/de.rs
@@ -3,3 +3,9 @@ use std::io;
 pub struct Deserializer<R: io::Read> {
     reader: R,
 }
+
+impl<R: io::Read> Deserializer<R> {
+    pub fn from_reader(reader: R) -> Deserializer<R> {
+        Deserializer { reader }
+    }
+}
