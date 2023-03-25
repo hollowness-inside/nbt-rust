aa84494bdb842788210389c320047bfdf2fab683
hollowness-inside <malikov.vp@students.dvfu.ru>
2023-03-25 10:13:33 -1000

    Update

--- examples/serialize.rs
+++ examples/serialize.rs
@@ -3,7 +3,9 @@ use std::io::Cursor;
 
 #[derive(serde::Serialize)]
 struct GameData {
-    abcde: u16
+    name: String,
+    version: u64,
+    player: Player,
 }
 
 #[derive(serde::Serialize)]
@@ -16,20 +18,16 @@ fn main() {
     let buffer: Vec<u8> = Vec::new();
     let mut writer = Cursor::new(buffer);
 
-    // let game_data = GameData {
-    //     name: "My Game".to_string(),
-    //     version: 1,
-    //     player: Player {
-    //         name: "Player 1".to_string(),
-    //         health: 100,
-    //     },
-    // };
-
     let game_data = GameData {
-        abcde: 63
+        name: "My Game".to_string(),
+        version: 1,
+        player: Player {
+            name: "Player 1".to_string(),
+            health: 100,
+        },
     };
 
     to_writer(&mut writer, &game_data).unwrap();
 
-    println!("{:#x?}", String::from_utf8(writer.into_inner()).unwrap());
+    println!("{:?}", String::from_utf8(writer.into_inner()).unwrap());
 }
